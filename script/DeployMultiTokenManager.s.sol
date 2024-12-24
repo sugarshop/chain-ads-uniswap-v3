feadb2866a70450892886f261a32b9b432bb9672
RQ <mengmengmengqiang@gmail.com>
2024-12-24 08:26:59 +0800

    feat: change english comment of deploy multitoken manager

--- script/DeployMultiTokenManager.s.sol
+++ script/DeployMultiTokenManager.s.sol
@@ -3,10 +3,10 @@ pragma solidity >=0.8.23 <0.9.0;
 
 import { MultiTokenManager } from "../src/MultiTokenManager.sol";
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import { BaseScript } from "./Base.s.sol";  // Add this import`
+import { BaseScript } from "./Base.s.sol";
 import { console } from "forge-std/src/console.sol";
 
-// 创建一个测试用的 ERC20 代币
+// create a test ERC20 token
 contract MEMEToken is ERC20 {
     constructor() ERC20("MEME Token", "MEME") {
         _mint(msg.sender, 1000000 * 10**18);
@@ -16,15 +16,15 @@ contract MEMEToken is ERC20 {
 /// @dev See the Solidity Scripting tutorial: https://book.getfoundry.sh/tutorials/solidity-scripting
 contract Deploy is BaseScript {
     function run() public broadcast returns (MultiTokenManager multiTokenManager) {
-        // 1. 部署 MultiTokenManager
+        // 1. deploy MultiTokenManager
         multiTokenManager = new MultiTokenManager();
         console.log("MultiTokenManager deployed at:", address(multiTokenManager));
 
-        // 2. 部署测试代币
+        // 2. deploy test token
         MEMEToken memeToken = new MEMEToken();
         console.log("MultiTokenManager deployed at:", address(memeToken));
 
-        // 3. 注册 ERC20代币  
+        // 3. register ERC20 token
         MultiTokenManager.TokenInfo memory tokenInfo = MultiTokenManager.TokenInfo({
             erc20Address: address(memeToken),
             name: "MEME Token",
@@ -35,20 +35,20 @@ contract Deploy is BaseScript {
         uint256 tokenId = multiTokenManager.registerToken(tokenInfo);
         console.log("Token registered with ID:", tokenId);
 
-        // 4. 铸造 ERC1155 币
+        // 4. mint ERC1155 token
         multiTokenManager.mint(msg.sender, tokenId, 1000 * 10**18, "");
         console.log("ERC1155 minted for 1000 MEME, token ID:", tokenId, " to:", msg.sender);
 
-        // 5. 销毁 ERC1155 币
+        // 5. burn ERC1155 token
         // multiTokenManager.burn(msg.sender, tokenId, 1000 * 10**18);
         multiTokenManager.burn(tokenId, 1000 * 10**18);
         console.log("ERC1155 burned for 1000 MEME, token ID:", tokenId, " from:", msg.sender);
 
-        // 6. 转移 ERC1155 币
+        // 6. transfer ERC1155 token
         multiTokenManager.safeTransferFrom(msg.sender, address(0x2), tokenId, 1000 * 10**18, "");
         console.log("ERC1155 transferred for 1000 MEME, token ID:", tokenId, " to:", address(0x2));
 
-        // 7. 兑换 ERC1155 币为 ERC20 币
+        // 7. swap ERC1155 token to ERC20 token
         uint256 amountOut = multiTokenManager.swapExactInputSingle(tokenId, address(memeToken), 500, 1000 * 10**18);
         console.log("ERC1155 swapped for", amountOut, "MEME, token ID:", tokenId);
     }
